345831c5d89011122596b7f8d40f2ad648bec6d8
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-08 12:51:22 -0500

    feat: Add color to squares on pointer enter
    This uses pointerenter instead of mouseenter to work whether it is a
    mouse, a stylus, or a finger. It simply changes the background color.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 container = document.getElementById("container");
+
 /* Makes the window a square based on the smaller of height or width. */
 (window.innerWidth < window.innerHeight) ?
     container.style.height = "100vw" :
@@ -8,11 +9,16 @@ numberSquares = 16;
 for (s = 0; s < numberSquares * numberSquares; s++) {
     square = document.createElement("div");
     square.classList.add("square");
+
     /* Define the squares' dimensions by the smaller of height or width. */
     (window.innerWidth < window.innerHeight) ?
         square.style.width = `${Math.floor(100 / numberSquares)}vw` :
         square.style.height = `${Math.floor(100 / numberSquares)}vh`
+
+    square.addEventListener("pointerenter", colorMe);
     container.appendChild(square);
 }
 
-console.log(`${Math.floor(100 / numberSquares)}%`)
+function colorMe(e) {
+    this.style.backgroundColor = "black";
+}
