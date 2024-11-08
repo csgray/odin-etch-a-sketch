e6cde4468c83c70ea23fc4d1aba9fb19b8ef025e
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-08 13:51:06 -0500

    feat: Add button to generate a new grid
    The grid is no longer static but generated on page load by a function
    that can be called again. There's an issue with rounding where the
    square is no longer square if their size rounds down too much.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,50 @@
-container = document.getElementById("container");
+/* Function Definitiions */
+function colorMe(e) {
+    this.style.backgroundColor = "black";
+}
 
-/* Makes the window a square based on the smaller of height or width. */
+function generateGrid(squaresPerSide) {
+    if (squaresPerSide > 100) squaresPerSide = 100;
+    numberSquares = squaresPerSide * squaresPerSide;
+
+    for (s = 0; s < numberSquares; s++) {
+        square = document.createElement("div");
+        square.classList.add("square");
+
+        /* Define the squares' dimensions by the smaller of height or width. */
+        (window.innerWidth < window.innerHeight) ?
+            square.style.width = `${Math.floor(100 / squaresPerSide)}vw` :
+            square.style.height = `${Math.floor(100 / squaresPerSide)}vh`
+
+        square.addEventListener("pointerenter", colorMe);
+        container.appendChild(square);
+    }
+}
+
+function removeSquares() {
+    squares = document.getElementsByClassName("square");
+
+    while (squares.length > 0) {
+        squares.item(0).remove();
+    }
+}
+
+function getNewGrid(e) {
+    const squaresPerSide = parseInt(prompt("How many squares per side?"));
+    if (isNaN(squaresPerSide)) return; // Abort if input is invalid
+
+    removeSquares();
+    generateGrid(squaresPerSide);
+}
+
+/* Page Logic */
+generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", getNewGrid);
+
+container = document.getElementById("container");
+/* Makes the grid a square based on the smaller of height or width. */
 (window.innerWidth < window.innerHeight) ?
     container.style.height = "100vw" :
     container.style.width = "100vh"
 
-numberSquares = 16;
-for (s = 0; s < numberSquares * numberSquares; s++) {
-    square = document.createElement("div");
-    square.classList.add("square");
-
-    /* Define the squares' dimensions by the smaller of height or width. */
-    (window.innerWidth < window.innerHeight) ?
-        square.style.width = `${Math.floor(100 / numberSquares)}vw` :
-        square.style.height = `${Math.floor(100 / numberSquares)}vh`
-
-    square.addEventListener("pointerenter", colorMe);
-    container.appendChild(square);
-}
-
-function colorMe(e) {
-    this.style.backgroundColor = "black";
-}
+generateGrid(16);
